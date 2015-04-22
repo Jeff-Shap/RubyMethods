c283dd85a13681e788dc2344db0517f44e7a0122
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-04-21 23:50:58 -0400

    Folder Script Backup

--- StockTicker/stock_ticker.rb
+++ StockTicker/stock_ticker.rb
@@ -1,14 +1,28 @@
 #stock_ticker.rb
+# puts "Please input the symbol of the stock you which to track: "
+# stock_to_track = gets.chomp
+
+require 'csv'
 require 'HTTParty'
 
-response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+def import
+	CSV.foreach("http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download", headers: true) do |row|
+		p o = row.to_hash
+	end
+end
 
-require 'nokogiri'
 
-dom = Nokogiri::HTML(response.body)
+import
 
-price_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+# response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
 
-live_price = price_span.first.content
-		puts "Apple stock is currently trading at #{price_span.first.content}"
+# require 'nokogiri'
+
+# dom = Nokogiri::HTML(response.body)
+
+# price_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+
+# live_price = price_span.first.content
+		
+# puts "Apple stock is currently trading at $#{price_span.first.content} per share"
 
